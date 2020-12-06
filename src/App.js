643f3030f01dea1dc90c4c7628e0704533898ea7
yoshiki-tanaka999 <shootingstarorufo@gmail.com>
2020-12-06 23:55:23 +0900

    CardListコンポーネントの作成

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+
 // import logo from './logo.svg';
 import './App.css';
 
@@ -25,9 +27,11 @@ class App extends Component {
   render() {
     return(
       <div className="App">
-        {this.state.monsters.map(monster => (
-            <h1 key= { monster.id } > { monster.name } </h1>
-          ))}
+        <CardList name="Yoshiki">
+          {this.state.monsters.map(monster => (
+                <h1 key= { monster.id } > { monster.name } </h1>
+              ))}
+        </CardList>
       </div>
     )
   }
