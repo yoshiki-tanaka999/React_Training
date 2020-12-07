2db12afae232f0fbb0380f40ceace4aa776102d8
yoshiki-tanaka999 <shootingstarorufo@gmail.com>
2020-12-07 23:25:04 +0900

    チュートリアル？完了

--- src/App.js
+++ src/App.js
@@ -34,6 +34,7 @@ class App extends Component {
 
     return(
       <div className="App">
+        <h1> Monster Rolodex </h1>
         <SearchBox
           placeholder= 'search monsters'
           handleChange= {e =>this.setState({ searchField: e.target.value }) }
